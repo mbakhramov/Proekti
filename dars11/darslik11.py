72004e8cb46d4f25ef6e561ec7f325906ceecf62
m.bakhramov <mirzairbakhramov@gmail.com>
2021-01-13 11:03:13 +0500

    Create darslik11.py
    dars11 yuklandi

---
+++ dars11/darslik11.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 13 10:59:52 2021
+
+@author: Mirzoir
+"""
+
+mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
+for mehmon in mehmonlar:
+    print("salom", mehmon)
+    print("xayir", mehmon)
